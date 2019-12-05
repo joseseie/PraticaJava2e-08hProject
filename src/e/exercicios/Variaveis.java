7b4e09d15c356d102789e6922b05b5c049c36d3c
Abdul-dotcom <Abdulrazak521.ar@gmail.com>
2019-12-05 10:24:58 +0200

    Exercicios Resolucao

---
+++ src/e/exercicios/Variaveis.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e.exercicios;
+
+/**
+ *
+ * @author _AlyyRazack
+ */
+public class Variaveis {
+    public static void main(String args[]){
+     //int numero =  "um";
+     int numero = 1;
+     // string 1 = "um";
+     String um = "1";
+     //float x = 0.2;
+     float x = 0.2f;
+     
+     // int idade.aluno = 18;
+     int idadeAluno = 18;
+     // int Integer = 2;
+     int integer = 2;
+     // double valor_pago = true
+     boolean valor_pago = true;
+     //boolean a = "true";
+     boolean a = true;
+     // String a = "125";
+      String v ="123";
+    }
+}
