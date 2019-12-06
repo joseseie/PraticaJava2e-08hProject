50983cda366a9e38d8074ab9e2da747c3a3a9925
Abdul-dotcom <Abdulrazak521.ar@gmail.com>
2019-12-06 08:48:21 +0200

    Todos exercicios resolvidos(Aula3)

---
+++ test/e/exercicios/Variaveis.java
@@ -0,0 +1,128 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author Fenias
+ */
+public class Variaveis{
+    public static void main(String[] args) {
+// 1. Analise as declaracoes e inicializacoess de variavies a baixo e faca alguma correcao se necessario
+    //int numero="um";
+        int numero = 1;    
+    //string 1="um";
+        String um ="um";
+    //float x="0.2":
+        float x = 0.2f;
+    //string x='nome';
+        String m="nome";
+    //string a="125";
+        String a="125";
+    //boolean z="true";
+        boolean z=true;
+    //double v=0,5,
+        double v =0.5;
+        String NomeAluno="Joao";
+    
+    //int idade.aluno=18;
+       int idadeAluno=18;
+       int Intenger=2;  
+    
+    //double valor_pago=true;
+      boolean valor_pago=true;
+        int numero1=2;
+        
+   // boolean _Valr=66
+        boolean _Valr=true;
+
+    /*
+       1.inicialize as varivais (nr, nr1, nr2) com tipos primitivos a sua escolha.
+          
+          double resultado = nr1 + nr1 + nr2;
+        
+         a) imprima resultado.
+         double nr1=2;
+          double nr2=3;
+          double nr3=4;
+          double resultado = nr1 + nr1 + nr2;
+          System.out.println(resultado); 
+    */
+            
+    /*
+       2.
+           saudacoes = "Öla, Mundo";
+           
+           a) imprima a String
+            String saudacoes = "Öla, Mundo";
+            System.out.print(saudacoes);
+            
+    */
+            
+    /*
+        3.
+            String logica = Ola, mundo;
+            
+           a) System.out.println (logica);
+           Que resultado sera apresentado?
+        `   R:Ola mundo.
+   
+    }
+  
+}
+
+            
+    /*
+        5.
+            Indique os tipos de classes Wrappers por ti conhecidas!
+            R:
+    */
+            
+    /*
+         6.   
+            a) Destes quais tu os consideras tipos primitivos:
+            
+                -for;
+                -else;
+                -if;
+                -char;
+                -short;
+                -while;
+                -do;
+                R:char e short.
+            
+            b) Pelos conhecimentos por ti adquiridos, quais consideras "Operadores"?
+                
+                - (
+                - )
+                - ^
+                - ///
+                R:nenhum
+            
+            
+         7. Considere os seguintes operacoes: +,-,*,%,++,--,==,!=,>, >=, <, <=, &&,||
+            
+            a) Indique os operadores de atribuicao, aritmeticos, relacionais, logicos, ncremento
+            decremento, igualdade, e a funcao de cada um.
+            R:operador de atribuição(=)
+            O operador de atribuição é utilizado para definir o valor inicial ou sobrescrever o valor de uma variável. 
+            Em seu uso, o operando à esquerda representa a variável para a qual desejamos atribuir o valor informado à direita.
+            #Operadores aritméticos(+,-,*,/ e %)
+            Os operadores aritméticos realizam as operações fundamentais da matemática entre duas variáveis e retornam o resultado. 
+            #Operadores de incremento e decremento(++,--)
+            #Operadores de igualdade(==,!=)
+            Os operadores de igualdade verificam se o valor ou o resultado da expressão lógica à esquerda é igual (“==”) ou diferente (“!=”) ao da direita, retornando um valor booleano.
+            #Operadores relacionais(<,>,>=,<=)
+            Os operadores relacionais, assim como os de igualdade, avaliam dois operandos.
+            #Operadores lógicos(&&-quando queremos que duas expressoes sejam verdadeiras e ||-quando precisamos que pelo menos uma delas)
+            Os operadores lógicos representam o recurso que nos permite criar expressões lógicas maiores a partir da junção de duas ou mais expressões.
+            
+            b)Indique a recedencia dos operadores.
+         
+    */        
+}
+}
