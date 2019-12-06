7ce0831efebcbbfdf0a8d5f4ba10c06d4c74fcea
Abdul-dotcom <Abdulrazak521.ar@gmail.com>
2019-12-06 09:08:37 +0200

    Update Exercicios4.java

--- src/e/exercicios/Exercicios4.java
+++ src/e/exercicios/Exercicios4.java
@@ -12,46 +12,57 @@ public class Exercicios4 {
         
         // 1. Analise as declaracoes e inicializacoess de variavies a baixo e faca alguma correcao se necessario
 
-        int numero="um";
+        //int numero="um";
+        int numero = 1;
 
-        string 1 ="um";
+        //string 1 ="um";
+        String um = "1";
 
-        float x="0.2":
+        //float x="0.2":
+        float x = 0.5f;
 
-        string x='nome';
+        //string x='nome';
+        String a = "nome";    
+        //string .ab.com ="site";
+        String abCom="site";
+        //boolean z="true";
+        boolean z = true;
+        //double v=0,5;
+        double v = 0.5;
 
-        string .ab.com ="site";
-
-        boolean z="true";
-
-        double v=0,5,
-
-        String NomeAluno="Joao"
-
-        int idade.aluno=18;
+        String NomeAluno="Joao";
+        
+        //int idade.aluno=18;
+        int idadeAluno = 18;
 
         String Jõao;
         
         int Intenger=2;
 
-        double valor_pago=true;
+        //double valor_pago=true;
+        boolean valor_pago=true;
 
-        int numero1=2,2;
+        //int numero1=2,2;
+        int numero1=2;
+        
 
-        boolean _Valr=66
-
-        Integer int = 10
+       // boolean _Valr=66
+        boolean valr= false;
+        //Integer int = 10;
+        int Integer = 10;
         
-        char char = 'K'
+        //char char = 'K'
+        char s='k';
         
-        char char;
+        //char char;
+        char p;
         
-        Doubles salario.
-        
-        String MEU_NOME = 'Esse é o meu nome'
-        
-        public long numeroDaConta = 123456789123456789;
-      
+        //Doubles salario.
+        double salario;
+        //String MEU_NOME = 'Esse é o meu nome'
+        String meuNome = "Esse e o meu nome";
+        //public long numeroDaConta = 123456789123456789;
+        long numeroDaconta = 123456789;
         
     }
     
