a961c06b22293c25011d51aa1db119fac96e7e77
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-06 09:33:47 +0200

    Pontos a serem melhorados
    Co-Authored-By: Fenias-Manhenge <fenias-manhenge@users.noreply.github.com>

--- src/e/exercicios/Exercicios4.java
+++ src/e/exercicios/Exercicios4.java
@@ -12,57 +12,45 @@ public class Exercicios4 {
         
         // 1. Analise as declaracoes e inicializacoess de variavies a baixo e faca alguma correcao se necessario
 
-        //int numero="um";
         int numero = 1;
 
-        //string 1 ="um";
         String um = "1";
 
-        //float x="0.2":
         float x = 0.5f;
 
-        //string x='nome';
         String a = "nome";    
-        //string .ab.com ="site";
+
         String abCom="site";
-        //boolean z="true";
+
         boolean z = true;
-        //double v=0,5;
+
         double v = 0.5;
 
-        String NomeAluno="Joao";
+        String NomeAluno="Joao"; //por melhorar
         
-        //int idade.aluno=18;
         int idadeAluno = 18;
 
-        String Jõao;
+        String Jõao; //por melhorar
         
-        int Intenger=2;
+        int Intenger=2; //por melhorar
 
-        //double valor_pago=true;
-        boolean valor_pago=true;
+        boolean valor_pago=true; //por melhorar
 
-        //int numero1=2,2;
         int numero1=2;
         
-
-       // boolean _Valr=66
         boolean valr= false;
-        //Integer int = 10;
-        int Integer = 10;
+
+        int Integer = 10; //por melhorar
         
-        //char char = 'K'
         char s='k';
         
-        //char char;
         char p;
         
-        //Doubles salario.
         double salario;
-        //String MEU_NOME = 'Esse é o meu nome'
+
         String meuNome = "Esse e o meu nome";
-        //public long numeroDaConta = 123456789123456789;
-        long numeroDaconta = 123456789;
+        
+        public long numeroDaConta = 123456789123456789;
         
     }
     
