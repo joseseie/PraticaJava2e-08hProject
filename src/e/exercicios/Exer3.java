fcab740971376ab931c68ab3606a8cf0f17cd946
Iben Enky <isimbine3@gmail.com>
2019-12-08 11:22:52 -0800

    Exercicio 3 resolvido

---
+++ src/e/exercicios/Exer3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e.exercicios;
+
+/**
+ *
+ * @author user
+ */
+public class Exer3 {
+   public static void main (String [] args){
+       
+   // 3.Escreva um pequeno programa que peça o seu nome, sua ocupação e ano de nascimento. Em seguida, imprima na tela as 2 informações a seguir:
+     //    Eu chamo me xxxxx de idade xxxxx anos de idade, estudante de xxxxx.
+
+             String nome="Iben";
+             int idade = 17;
+             String ocupacao="JAVA";
+             System.out.println("Eu chamo me");
+             System.out.print(nome);
+             System.out.print("de idade");
+             System.out.print(idade);
+             System.out.print("anos de idade");
+             System.out.print(",estuddante de");
+             System.out.print(ocupacao);
+                  
+             
+   } 
+}
