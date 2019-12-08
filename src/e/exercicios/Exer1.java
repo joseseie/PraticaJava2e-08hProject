549c8181c4ddf3c97da37002e5bdead171ada95e
Iben Enky <isimbine3@gmail.com>
2019-12-08 11:04:47 -0800

    Exercicio 1 Resolvido

---
+++ src/e/exercicios/Exer1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e.exercicios;
+
+/**
+ *
+ * @author user
+ */
+public class Exer1 {
+    public static void main(String[] args){
+        
+      //   1.	Faça um programa que imprima o texto “Olá  mundo" para o usuário. Em seguida, imprima na tela o seu código de visita da plataforma Explicador e então, imprima uma linha em branco. Depois de imprimir a linha em branco, escreva na tela a seguinte frase:
+        //       "Seja bem-vindo ao curso de Java Básico Intensivo"      
+      
+        System.out.println("Ola mundo");
+        System.out.println("11203901239029390");
+            System.out.println("");
+            System.out.println("Seja bem-vindo ao curso de Java Basico Intensivo");
+    }
+}
