b6ad6de567b471d2d79a79b315db34c5a6c173ab
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-09 10:43:02 +0200

    Algoritmo para e3

--- src/e/correcoes/e5/Exer3.java
+++ src/e/correcoes/e5/Exer3.java
@@ -1,30 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package e.correcoes.e5;
 
 /**
  *
- * @author user
+ * @author Iben Enky
+ * @author José Seie
+ * @author Nicolas Muchate
+ * @author Hassan Mutola
+ * 
  */
 public class Exer3 {
    public static void main (String [] args){
        
-   // 3.Escreva um pequeno programa que peça o seu nome, sua ocupação e ano de nascimento. Em seguida, imprima na tela as 2 informações a seguir:
-     //    Eu chamo me xxxxx de idade xxxxx anos de idade, estudante de xxxxx.
+   // 3.Escreva um pequeno programa que peça o seu nome, sua ocupação e ano de nascimento. 
+   // Em seguida, imprima na tela as 2 informações a seguir:
+   // Eu chamo me xxxxx de idade xxxxx anos de idade, estudante de xxxxx.
 
-             String nome="Iben";
-             int idade = 17;
-             String ocupacao="JAVA";
-             System.out.println("Eu chamo me");
-             System.out.print(nome);
-             System.out.print("de idade");
-             System.out.print(idade);
-             System.out.print("anos de idade");
-             System.out.print(",estuddante de");
-             System.out.print(ocupacao);
+   // # Algorítmo:
+   //1. Ler o nome do usuário
+   //2. Ler a ocupação do usuário
+   //3. Ler o ano de nascimento
+   //4. Imprimir as informações usando o formato sugerido.
+   
+    String nome="Iben";
+    int idade = 17;
+    String ocupacao="JAVA";
+    System.out.println("Eu chamo me");
+    System.out.print(nome);
+    System.out.print("de idade");
+    System.out.print(idade);
+    System.out.print("anos de idade");
+    System.out.print(",estuddante de");
+    System.out.print(ocupacao);
                   
              
    } 
