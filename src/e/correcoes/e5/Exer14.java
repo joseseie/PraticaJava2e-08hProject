974c45279e223855db6c2468f16b7c900d39fb65
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-09 11:34:46 +0200

    Exercicío 14 deixando pendente.

--- src/e/correcoes/e5/Exer14.java
+++ src/e/correcoes/e5/Exer14.java
@@ -1,18 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package e.correcoes.e5;
 
 /**
  *
- * @author user
+ * @author José Seie
+ * @author Iben Enky
+ * @author Nicolas Muchate
+ * @author Hassan Mutola
+ * 
  */
 public class Exer14 {
     public static void main (String[] args){
     
+    /*
+        14 Escreva uma classe que contenha métodos estáticos para calcular a conversão entre diferentes escalas de temperatura. Considere as fórmulas de conversão abaixo: 
+        a)	De graus Celsius (C) para graus Fahrenheit (F): F = (9 × C/5) + 32) 
+        b)	De graus Fahrenheit (F) para graus Celsius (C): C = (F − 32) × +5/9 
+        c)	De graus Celsius (C) para graus Kelvin (K): K = C + 273.15 
+        d)	De graus Kelvin (K) para graus Celsius (C): C = K − 273.15 
+        e)	De graus Celsius (C) para graus Réaumur (Re): Re = C ∗ 4/5 
+        f)	De graus Réaumur (Re) para graus Celsius (C): C = Re ∗ 5/4 
+        g)	De graus Kelvin (K) para graus Rankine (R): R = K ∗ 1.8 
+        h)	De graus Rankine (R) para graus Kelvin (K): K = R/1.8
+        */
     
-        
+    // TPC para a lição 06
+    
+    
+    
     }
 }
