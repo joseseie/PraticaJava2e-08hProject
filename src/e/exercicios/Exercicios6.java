553a010c029166695334654e13eb7253d03b6804
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-09 11:51:47 +0200

    Create Exercicios6.java

---
+++ src/e/exercicios/Exercicios6.java
@@ -0,0 +1,25 @@
+
+package e.exercicios;
+
+/**
+ *
+ * @author joseseie
+ * @author Iben Enky
+ * @author Nicolas Muchate
+ * @author Hassan Mutola
+ * 
+ */
+public class Exercicios6 {
+    
+    public static void main(String[] args) {
+        
+        for (int i =0; i< 10; i++) {
+             int i = 0;
+        }
+        
+        foreach (int i =0; i< 10; i++) {
+             int i = 0;
+        }
+    }
+    
+}
