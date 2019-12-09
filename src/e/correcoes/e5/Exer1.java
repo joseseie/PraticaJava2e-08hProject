6990f7ae4da308781a57314ff4455d1edf14ae67
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-09 10:24:33 +0200

    Exercício 01 verificado e aprovado.

--- src/e/correcoes/e5/Exer1.java
+++ src/e/correcoes/e5/Exer1.java
@@ -12,16 +12,15 @@ package e.correcoes.e5;
 public class Exer1 {
     public static void main(String[] args){
         
-      //   1.	Faça um programa que imprima o texto “Olá  mundo" para o usuário. Em seguida, imprima na tela o seu código de visita da plataforma Explicador e então, imprima uma linha em branco. Depois de imprimir a linha em branco, escreva na tela a seguinte frase:
-        //       "Seja bem-vindo ao curso de Java Básico Intensivo"      
+        //   1.	Faça um programa que imprima o texto “Olá  mundo" para o usuário. Em seguida, imprima na tela o seu código de visita da plataforma Explicador e então, imprima uma linha em branco. Depois de imprimir a linha em branco, escreva na tela a seguinte frase:
+        //   "Seja bem-vindo ao curso de Java Básico Intensivo"      
       
         // 1. Imprimir a mensagem "Olá Mundo."
         // 2. Imprimir na tela o código de visita da plataforma Explicador
         // 3. Imprimir uma linha em branco
         // 4. Imprimir a mensagem "Seja bem-vindo ao curso de Java Básico Intensivo"
         System.out.println("Ola mundo");
-        System.out.println("11203901239029390");
-            System.out.println("");
-            System.out.println("Seja bem-vindo ao curso de Java Basico Intensivo");
+        System.out.println("#7\n"); //Código de visita de José Seie
+        System.out.println("Seja bem-vindo ao curso de Java Basico Intensivo");
     }
 }
