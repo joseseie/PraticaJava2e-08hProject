eb52e21434a830963c929ffc7bbedb2b7d4bfb6a
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-13 11:10:43 +0200

    Criação da classe Lampada para o exercício 01 da ficha de Objecto

---
+++ src/e/correcoes/e8/oo/e1/Lampada.java
@@ -0,0 +1,22 @@
+
+package e.correcoes.e8.oo.e1;
+
+/**
+ *
+ * @author joseseie
+ * @author Horcha Chambule
+ * @author Abdul Rasac
+ * @author Francisco Domalamo
+ * @author Nicolas Muchate
+ * @author Hassan Mutola
+ * 
+ */
+public class Lampada {
+    
+    private String cor;
+    private boolean estado;
+    private String tipo;
+    private double voltagem;
+    private byte grauDeLuminosidade;
+    
+}
